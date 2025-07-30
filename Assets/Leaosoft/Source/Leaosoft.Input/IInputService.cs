c31d5b4f2ad043b91a1cba75eee45651daa0015c
Wendell Leão <leaowendell@outlook.com>
2025-07-30 12:03:18 -0300

    chore: import the leaosoft framework repository

---
+++ Assets/Leaosoft/Source/Leaosoft.Input/IInputService.cs
@@ -0,0 +1,12 @@
+﻿using Leaosoft.Services;
+using UnityEngine;
+
+namespace Leaosoft.Input
+{
+    public interface IInputService : IGameService
+    {
+        public Vector2 GetPlayerMovement();
+        public Vector2 GetMouseDelta();
+        public bool GetPlayerJumpedThisFrame();
+    }
+}
