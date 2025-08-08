8b29a06d6c75d3f835d994654a74b3d93d7f2cb5
Wendell Leão <leaowendell@outlook.com>
2025-08-08 08:51:11 -0300

    feat: implement enemy respawn

--- Assets/_Project/Source/Fantasy.Gameplay/Enemies/Manager/EnemyManager.cs
+++ Assets/_Project/Source/Fantasy.Gameplay/Enemies/Manager/EnemyManager.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Fantasy.Events.Health;
 using Fantasy.Domain.Health;
+using Leaosoft;
 using Leaosoft.Events;
 using UnityEngine;
 
@@ -13,10 +17,13 @@ namespace Fantasy.Gameplay.Enemies.Manager
         private Transform spawnPoint;
         [SerializeField]
         private float destroyEnemyObjectDelay = 7f;
+        [SerializeField]
+        private float respawnEnemyTimer = 2.5f;
 
         private IParticleFactory _particleFactory;
         private IWeaponFactory _weaponFactory;
         private IEventService _eventService;
+        private CancellationTokenSource _destroyEnemyObjectCts;
 
         public void Initialize(IParticleFactory particleFactory, IWeaponFactory weaponFactory, IEventService eventService)
         {
@@ -30,13 +37,41 @@ namespace Fantasy.Gameplay.Enemies.Manager
         protected override void OnInitialize()
         {
             base.OnInitialize();
+
+            SpawnEnemy();
+        }
+
+        protected override void OnDispose()
+        {
+            base.OnDispose();
             
+            _destroyEnemyObjectCts?.Cancel();
+        }
+
+        protected override void DisposeEntity(IEntity entity)
+        {
+            base.DisposeEntity(entity);
+
+            if (entity is not BasicEnemy basicEnemy)
+            {
+                return;
+            }
+            
+            basicEnemy.OnDied -= HandleBasicEnemyDied;
+
+            _destroyEnemyObjectCts?.Cancel();
+            _destroyEnemyObjectCts = new CancellationTokenSource();
+            
+            DestroyEnemyObjectAsync(basicEnemy.gameObject, _destroyEnemyObjectCts.Token).Forget();
+        }
+
+        private void SpawnEnemy()
+        {
             BasicEnemy basicEnemy = (BasicEnemy)CreateEntity(enemyPrefab, spawnPoint);
 
             basicEnemy.Initialize(_particleFactory, _weaponFactory);
             basicEnemy.Begin();
 
-            //TODO: PROPERLY UNSUBSCRIBE FROM ALL EVENTS ON DISPOSE
             basicEnemy.OnDied += HandleBasicEnemyDied;
             
             DispatchDamageableSpawnedEvent(basicEnemy.Damageable);
@@ -44,16 +79,42 @@ namespace Fantasy.Gameplay.Enemies.Manager
 
         private void HandleBasicEnemyDied(BasicEnemy basicEnemy)
         {
-            basicEnemy.OnDied -= HandleBasicEnemyDied;
-            
             DisposeEntity(basicEnemy);
-            
-            Destroy(basicEnemy.gameObject, destroyEnemyObjectDelay);
         }
-
+        
         private void DispatchDamageableSpawnedEvent(IDamageable enemyDamageable)
         {
             _eventService.DispatchEvent(new DamageableSpawnedEvent(enemyDamageable));
+        }
+        
+        private async UniTask DestroyEnemyObjectAsync(GameObject basicEnemyGameObject, CancellationToken token)
+        {
+            try
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(destroyEnemyObjectDelay), cancellationToken: token);
+
+                if (!basicEnemyGameObject || token.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                Destroy(basicEnemyGameObject);
+
+                await UniTask.Delay(TimeSpan.FromSeconds(respawnEnemyTimer), cancellationToken: token);
+
+                SpawnEnemy();
+            }
+            catch (OperationCanceledException)
+            { }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+            finally
+            {
+                _destroyEnemyObjectCts?.Dispose();
+                _destroyEnemyObjectCts = null;
+            }
         }
     }
 }
